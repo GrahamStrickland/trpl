6ea7119833e587f89fbff37a0ab234f9cbb8448d
Graham Strickland <grahamhstrickland@gmail.com>
2024-09-01 19:57:06 +0200

    Add methods to Rectangle struct

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,24 +4,41 @@ struct Rectangle {
     height: u32,
 }
 
-fn main() {
-    let rect1 = Rectangle {
-        width: 30, 
-        height: 50
-    };
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 
-    println!("The are of the rectangle is {} square pixels.", area(&rect1));
-    println!("rect1 is {rect1:#?}");
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 
-    let scale = 2;
-    let rect2 = Rectangle {
-        width: dbg!(30 * scale),
-        height: 50,
-    };
-
-    dbg!(&rect2);
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(3);
+
+    println!("Area of sq {sq:#?} = {}", sq.area());
 }
