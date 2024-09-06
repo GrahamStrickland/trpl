81e51266bc5e0e94600149439fcdf6a419638b18
Graham Strickland <grahamhstrickland@gmail.com>
2024-09-06 21:12:12 +0200

    Add coin match example

---
+++ coin_match/src/main.rs
@@ -0,0 +1,22 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn main() {
+    println!("Value of 1 penny = {}c", value_in_cents(Coin::Penny));
+    println!("Value of 1 nickel = {}c", value_in_cents(Coin::Nickel));
+    println!("Value of 1 dime = {}c", value_in_cents(Coin::Dime));
+    println!("Value of 1 quarter = {}c", value_in_cents(Coin::Quarter));
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
