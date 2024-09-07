f3b98598b1ef3cdb4503ea9cd68ed718bb2985c8
Graham Strickland <grahamhstrickland@gmail.com>
2024-09-07 12:41:05 +0200

    Add US state enum to coin enum example

--- coin_match/src/main.rs
+++ coin_match/src/main.rs
@@ -1,15 +1,22 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn main() {
     println!("Value of 1 penny = {}c", value_in_cents(Coin::Penny));
     println!("Value of 1 nickel = {}c", value_in_cents(Coin::Nickel));
     println!("Value of 1 dime = {}c", value_in_cents(Coin::Dime));
-    println!("Value of 1 quarter = {}c", value_in_cents(Coin::Quarter));
+    println!("Value of 1 quarter = {}c", value_in_cents(Coin::Quarter(UsState::Alaska)));
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -20,6 +27,9 @@ fn value_in_cents(coin: Coin) -> u8 {
         }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
     }
 }
