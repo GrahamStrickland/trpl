8b0f15dedcec9d6be08115228d124a602d8d560b
Graham Strickland <grahamhstrickland@gmail.com>
2024-09-08 19:49:32 +0200

    Add separate files for modules in restaurant example

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,21 +1,7 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
 
 mod customer {
-    use crate::front_of_house::hosting;
+    pub use crate::front_of_house::hosting;
 
     pub fn eat_at_restaurant() {
         // crate::front_of_house::hosting::add_to_waitlist();
