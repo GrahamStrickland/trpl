f633b915880ed04c2bbb116f787a3fb1d52c7c18
Graham Strickland <grahamhstrickland@gmail.com>
2024-10-20 14:13:30 +0200

    Add string input to departments exercise

--- departments/src/main.rs
+++ departments/src/main.rs
@@ -1,18 +1,68 @@
 use std::collections::HashMap;
+use std::io;
 
 fn main() {
-    let employee = String::from("Sally");
-    let mut department = String::from("Engineering");
     let mut departments = HashMap::new();
 
-    add_employee(employee, department, &mut departments);
+    loop {
+        let department = String::from("Engineering");
+        println!("Please enter an employee to add to {department}.");
 
-    department = String::from("Engineering");
-    println!("Employees in {department}:");
-    let employee_list = retrieve_employee_list(department, &mut departments);
-    match employee_list {
-        Some(employee_list) => print_employee_list(employee_list),
-        None => println!("No matching employees found!"),
+        let mut employee_line = String::new();
+        io::stdin()
+            .read_line(&mut employee_line)
+            .expect("Failed to read line");
+
+        let employee = String::from(employee_line.trim_ascii());
+        add_employee(employee, department, &mut departments);
+
+        println!("Add another employee (Y/N)?");
+        let mut answer = String::new();
+        io::stdin()
+            .read_line(&mut answer)
+            .expect("Failed to read line");
+
+        match answer.as_str().trim_ascii() {
+            "Y" | "y" => continue,
+            "N" | "n" => break,
+            _ => {
+                println!("Invalid response, please enter 'Y' for yes or 'N' for no.");
+                continue;
+            }
+        }
+    }
+
+    loop {
+        println!("Please enter a department for which you would like to view the employees.");
+
+        let mut department_line = String::new();
+        io::stdin()
+            .read_line(&mut department_line)
+            .expect("Failed to read line");
+
+        let department = String::from(department_line.trim_ascii());
+
+        println!("Employees in {department}:");
+        let employee_list = retrieve_employee_list(department, &mut departments);
+        match employee_list {
+            Some(employee_list) => print_employee_list(employee_list),
+            None => println!("No matching employees found!"),
+        }
+
+        println!("Print another department (Y/N)?");
+        let mut answer = String::new();
+        io::stdin()
+            .read_line(&mut answer)
+            .expect("Failed to read line");
+
+        match answer.as_str().trim_ascii() {
+            "Y" | "y" => continue,
+            "N" | "n" => break,
+            _ => {
+                println!("Invalid response, please enter 'Y' for yes or 'N' for no.");
+                continue;
+            }
+        }
     }
 }
 
@@ -21,7 +71,7 @@ fn add_employee(
     department: String,
     departments: &mut HashMap<String, Vec<String>>,
 ) {
-    let employee_list = departments.get(&department);
+    let employee_list = departments.get_mut(&department);
 
     match employee_list {
         Some(employee_list) => employee_list.push(employee),
@@ -46,7 +96,10 @@ fn retrieve_employee_list(
 }
 
 fn print_employee_list(employees: Vec<String>) {
-    for employee in employees {
+    let mut sorted_employees = employees.clone();
+    sorted_employees.sort();
+
+    for employee in sorted_employees {
         println!("{employee}");
     }
 }
