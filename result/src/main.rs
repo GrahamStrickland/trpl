526771edcc386547f65bd3827f86e10c164d8864
Graham Strickland <grahamhstrickland@gmail.com>
2024-10-26 11:49:08 +0200

    Add ? operator example to result example

--- result/src/main.rs
+++ result/src/main.rs
@@ -1,5 +1,5 @@
-use std::fs::File;
-use std::io::{self, Read};
+use std::fs;
+use std::io;
 
 fn main() {
     let result = read_username_from_file();
@@ -11,17 +11,5 @@ fn main() {
 }
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let username_file_result = File::open("hello.txt");
-
-    let mut username_file = match username_file_result {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
-
-    let mut username = String::new();
-
-    match username_file.read_to_string(&mut username) {
-        Ok(_) => Ok(username),
-        Err(e) => Err(e),
-    }
+    fs::read_to_string("hello.txt")
 }
