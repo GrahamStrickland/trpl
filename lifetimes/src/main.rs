89a4fa5d8ef3ed100f8cbd55c577035a5aa51b63
Graham Strickland <grahamhstrickland@gmail.com>
2024-11-03 12:38:00 +0200

    Add more to lifetimes example

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,3 +1,40 @@
+use std::fmt::Display;
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {announcement}");
+        self.part
+    }
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {ann}");
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn main() {
     let string1 = String::from("long string is long");
 
@@ -6,12 +43,23 @@ fn main() {
         let result = longest(string1.as_str(), string2.as_str());
         println!("The longest string is {result}");
     }
-}
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().unwrap();
+    println!("first_sentence: {first_sentence}");
+
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+    let level = i.level();
+    println!("i.level(): {level}");
+    i.announce_and_return_part(first_sentence);
+
+    let longest = longest_with_an_announcement(
+        string1.as_str(),
+        novel.split('.').next().unwrap(),
+        first_sentence,
+    );
+    println!("longest: {longest}");
 }
