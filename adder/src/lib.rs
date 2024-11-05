31a9c784b3cdadaf8f9f69e890f65fd627c68ada
Graham Strickland <grahamhstrickland@gmail.com>
2024-11-05 18:58:51 +0200

    Add more test examples

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -38,6 +38,11 @@ impl Guess {
     }
 }
 
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {a}");
+    10
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -88,6 +93,24 @@ mod tests {
     }
 
     #[test]
+    fn add_two_and_two() {
+        let result = add_two(2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        let result = add_two(3);
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn one_hundred() {
+        let result = add_two(100);
+        assert_eq!(result, 102);
+    }
+
+    #[test]
     fn greeting_contains_name() {
         let result = greeting("Carol");
         assert!(
@@ -100,5 +123,18 @@ mod tests {
     #[should_panic(expected = "less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
+    }
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(value, 10);
+    }
+
+    #[test]
+    #[ignore]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(value, 5);
     }
 }
