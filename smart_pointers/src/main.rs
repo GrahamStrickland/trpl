96ca83026d102d26b7db45960eaf5a35b968b2b3
Graham Strickland <grahamhstrickland@gmail.com>
2025-02-10 21:31:00 +0200

    Add reference cycle examples

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,8 +1,19 @@
+use std::cell::RefCell;
 use std::rc::Rc;
 
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(i32, RefCell<Rc<List>>),
     Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
 }
 
 use std::ops::Deref;
@@ -36,12 +47,12 @@ impl Drop for CustomSmartPointer {
 }
 
 fn main() {
-    let list = Rc::new(Cons(1, Rc::new(Cons(2, Rc::new(Cons(3, Rc::new(Nil)))))));
-    let x = 5;
-    let y = MyBox::new(x);
+    // let list = Rc::new(Cons(1, Rc::new(Cons(2, Rc::new(Cons(3, Rc::new(Nil)))))));
+    // let x = 5;
+    // let y = MyBox::new(x);
 
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
+    // assert_eq!(5, x);
+    // assert_eq!(5, *y);
 
     let m = MyBox::new(String::from("Rust"));
     hello(&m);
@@ -56,18 +67,49 @@ fn main() {
     drop(c);
     println!("CustomSmartPointer dropped before the end of main.");
 
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
-    }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    // let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    // println!("count after creating a = {}", Rc::strong_count(&a));
+    // let b = Cons(3, Rc::clone(&a));
+    // println!("count after creating b = {}", Rc::strong_count(&a));
+    // {
+    //     let c = Cons(4, Rc::clone(&a));
+    //     println!("count after creating c = {}", Rc::strong_count(&a));
+    // }
+    // println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 
-    let x = 5;
-    let y = &mut x;
+    // let x = 5;
+    // let y = &mut x;
+    // let value = Rc::new(RefCell::new(5));
+
+    // let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    // let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    // let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    // *value.borrow_mut() += 10;
+
+    // println!("a after = {a:?}");
+    // println!("b after = {b:?}");
+    // println!("c after = {c:?}");
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changin a = {}", Rc::strong_count(&a));
+
+    println!("a next item = {:?}", a.tail());
 }
 
 fn hello(name: &str) {
