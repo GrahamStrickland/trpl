f7f76a1ade88daf7a509fb255c56906f7acf63c7
Graham Strickland <grahamhstrickland@gmail.com>
2025-03-12 18:36:45 +0200

    Add mutex examples

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::mpsc;
+use std::sync::{mpsc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -56,4 +56,31 @@ fn main() {
     for received in rx {
         println!("Got: {received}");
     }
+
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    println!("m = {m:?}");
+
+    let counter = Mutex::new(0);
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
