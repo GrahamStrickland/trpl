0ac220fe11609e3d4d00b8321a6f4f8b1074697e
Graham Strickland <grahamhstrickland@gmail.com>
2025-03-16 17:50:29 +0200

    Add Arc example

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::{mpsc, Mutex};
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -66,10 +66,11 @@ fn main() {
 
     println!("m = {m:?}");
 
-    let counter = Mutex::new(0);
+    let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
 
     for _ in 0..10 {
+        let counter = Arc::clone(&counter);
         let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
 
